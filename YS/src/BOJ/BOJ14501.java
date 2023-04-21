6ae59d5b9cab886a73b305f5e756445f969731f8
yawnsoo <0607yeonsoo@gmail.com>
2023-04-21 15:05:30 +0900

    feat : BOJ-14501

---
+++ YS/src/BOJ/BOJ14501.java
@@ -0,0 +1,53 @@
+package BOJ;
+import java.util.*;
+
+public class BOJ14501 {
+
+        static int answer = 0;
+        boolean[] check;
+
+
+        private void dfs(int n, List<int[]> schedule, int day, int total, int last){
+            //종료 조건 : 종료 날짜
+            if(day>n-1) { //  - 마지막 날
+                if(day==n){
+                    answer = Math.max(answer,total);
+                }else{ //  - 마지막날 초과 : 초과한 날짜 빼기
+                    answer = Math.max(answer,total-last);
+                }
+
+                return;
+            }
+
+            for (int i = day; i < n; i++) {
+                if(!check[i]){
+                    check[i] = true;
+                    dfs(n, schedule, i+schedule.get(i)[0], total+schedule.get(i)[1], schedule.get(i)[1]);
+                    check[i] = false;
+                }
+            }
+
+        }
+
+        public int solution(int n, List<int[]> schedule){
+
+            check = new boolean[n];
+
+            dfs(n, schedule, 0, 0,0);
+
+            return answer;
+        }
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            List<int[]> schedule = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                int t = sc.nextInt();
+                int p = sc.nextInt();
+                schedule.add(new int[]{t,p});
+            }
+
+            System.out.println(new BOJ14501().solution(n, schedule));
+        }
+}
--- YS/src/Main.java
+++ YS/src/Main.java
@@ -2,44 +2,50 @@ import java.util.*;
 
 public class Main {
 
-    public long solution(int n, int[] room, int b, int c){
-        long answer = 0;
+    static int answer = 0;
+    boolean[] check;
 
-//      각각의 시험장에 총감독관은 오직 1명만 있어야 하고
-        for (int i = 0; i < n; i++) {
-            room[i] -= b;
-            answer++;
+
+    private void dfs(int n, List<int[]> schedule, int day, int total, int last){
+        //종료 조건 : 종료 날짜
+        if(day>n-1) { //  - 마지막 날
+            if(day==n){
+                answer = Math.max(answer,total);
+            }else{ //  - 마지막날 초과 : 초과한 날짜 빼기
+                answer = Math.max(answer,total-last);
+            }
+            return;
         }
-//      부감독관은 여러 명 있어도 된다.
-        for (int i = 0; i < n; i++) {
-            if(room[i]>0){
-                answer += Math.ceil(room[i]/(float)c);
+
+        for (int i = day; i < n; i++) {
+            if(!check[i]){
+                check[i] = true;
+                dfs(n, schedule, i+schedule.get(i)[0], total+schedule.get(i)[1], schedule.get(i)[1]);
+                check[i] = false;
             }
         }
 
+    }
 
-//        출력
-//        각 시험장마다 응시생을 모두 감독하기 위해 필요한 감독관의 최소 수를 출력한다.
-//        주어진 문제에서 answer는 최대 1,000,000 * 1,000,000 = 1,000,000,000,000
-//        int의 범위 는 -2147483648 ~ 2,147,483,647
-//        long의 범위는 -9223372036854775808 ~ 9,223,372,036,854,775,807
-//        그러므로, answer의 자료형은 long이여야 한다.
+    public int solution(int n, List<int[]> schedule){
+
+        check = new boolean[n];
+
+        dfs(n, schedule, 0, 0,0);
+
         return answer;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
-        int[] room = new int[n];
+        List<int[]> schedule = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            int a = sc.nextInt();
-            room[i] = a;
+            int t = sc.nextInt();
+            int p = sc.nextInt();
+            schedule.add(new int[]{t,p});
         }
-        int b = sc.nextInt();
-        int c = sc.nextInt();
 
-
-        System.out.println(new Main().solution(n,room,b,c));
+        System.out.println(new Main().solution(n, schedule));
     }
 }
